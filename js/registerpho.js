73757e087c8e14e1f5b55851d2ab0c4a7246e822
yanglili-hub <1290177572@qq.com>
2019-10-15 13:53:18 +0800

    完成检测密码前后是否一致

--- js/registerpho.js
+++ js/registerpho.js
@@ -1,66 +1,72 @@
-非空判断
+let zhuceya=document.getElementsByClassName('zhuceya');
 
-function reg(){
+zhuceya[0].onclick=function(){
+    // 非空判断
     let telphone=document.getElementById('telphone');
     let createPass=document.getElementById('createPass');
-    if(telphone.value==''||createPass.value==''||querenPass.val==''){
-        alert('输入的内容不能为空');
+    let querenPass=document.getElementById('querenPass');
+    let telphoneMes=document.getElementById('telphoneMes')
+    let passya=document.getElementById('passya');
+    let spans=document.getElementsByClassName('spans');
+    let qiangdu=document.getElementById("qiangdu");
+    let weak=document.getElementById('weak');
+    let middle=document.getElementById('middle');
+    let strong=document.getElementById('strong');
+    if(telphone.value==''||createPass.value==''||querenPass.value==''){
+        alert('亲，输入的内容不能为空');
         return false;
     }
-}
-    
+    // 确认两次输入密码是否一致
+    if(querenPass.value!=createPass.value){
+        passya.innerHTML='两次密码输入不一样';
+        return false;
+        }else{
+        passya.innerHTML='';
 
-
-// 匹配1开头的11位手机号码
-// 手机号判断
-    var ph=/^1[3|5|7|8|][0-9]{9}$/
-    if($("#telphone").val()!=""){
-        if(!(ph.test($("#telphone").val()))){
-            $("#telphoneMes").text("请输入正确手机号");
-            return false;
-        }else if(ph){
-            $("#telphoneMes").text("");
+        }
+    // 匹配1开头的11位手机号码
+    // 手机号判断
+    var phreg=/^1[3|5|7|8|][0-9]{9}$/
+    if(telphone.value!=""){
+        if(phreg.test(telphone.value)){
+            spans[0].style.background=`url(../img/registerimg/error.gif) no-repeat -38px -17px`;
+            
+            alert('dui');
             return true;
+        }else {
+            spans[0].style.background=`url(../img/registerimg/error.gif) no-repeat -7px -17px`;
+            telphoneMes.innerHTML="请输入正确的手机号";
+            return false;
         }
     }else{
-            $("#telphoneMes").text("");
-        }
-    
+        alert('请输入手机号');
+        telphoneMes.innerHTML('请输入手机号')
+        }   
+}
 
-$("form-group input").blur(function(){
-    $(this).css('border',"black");
-})
 // 边输入边显示密码强度
-$("#col-sm-10 #createPass").blur(function(){
-    if($("#createPass").val=''){
-        $("#qiangdu").show();
-        $("#qiangdu").val='';
-        $("#qiangdu").val='请输入6-16位的数字密码';
+createPass.onblur=function(){
+    if(createPass.value=''){
+        qiangdu.innerHTML='请输入6-16位的数字密码';
     }else{
         $("#qiangdu").show();
-        $("#weak")[0].style.backgroundColor='';
-        $("#middle")[0].style.backgroundColor='';
-        $("#strong")[0].style.backgroundColor='';
-        if($(this).val.length==0){
+        weak.style.backgroundColor='';
+        middle.style.backgroundColor='';
+        strong.style.backgroundColor='';
+        if(this)value.length==0){
 
         }else if($(this).val.length<=6){
-            $("#weak")[0].style.backgroundColor='#red';
+            $("#weak")[0].style.backgroundColor='#ff464e';
         }else if($(this).val.length<=10){
-            $("#middle")[0].style.backgroundColor='#red';
+            $("#middle")[0].style.backgroundColor='#ff464e';
         }else if($(this).val.length<=16){
-            $("#strong")[0].style.backgroundColor='#red';
+            $("#strong")[0].style.backgroundColor='#ff464e';
         }
     }
     
-})
+}
 // 确认密码
-$('#querenPass').keyup(function(){
-    if($(this).val==''){
-        $('#querenPass + .message').html='请再次输入密码';
-    }else if($(this).val!==$('#createPass').val){
-        $('#querenPass + .message').html='两次密码输入不一样';
-    }
-})
+
 //验证码
 $("#yanZhengMa").click(function(){
     // $('#querenPass + .message').show();
